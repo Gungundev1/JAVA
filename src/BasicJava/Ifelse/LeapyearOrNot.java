0f26080631a22fb09abca4a804116d7e3a92b8dc
Gungun agrawal <agrawalgungun859@gmail.com>
2024-03-28 17:13:32 +0530

    table

--- src/BasicJava/Ifelse/LeapyearOrNot.java
+++ src/BasicJava/Ifelse/LeapyearOrNot.java
@@ -6,6 +6,7 @@ public class LeapyearOrNot {
     public static void main(String[] args) {
         //To check year is leap year or not
         Scanner input=new Scanner(System.in);
+        System.out.println("Enter any year:");
         int year= input.nextInt();
         if(year%4==0||(year%400==0&&year%4==0)){
             System.out.println(year+" is leap year");
---
+++ src/BasicJava/Loops.java
@@ -0,0 +1,16 @@
+package BasicJava;
+
+import java.util.Scanner;
+
+public class Loops {
+    public static void main(String[] args) {
+        // To generate multiplication table
+        Scanner input=new Scanner(System.in);
+        System.out.println("what number table do you want to generate ");
+        int num= input.nextInt();
+        for(int i=1;i<=10;i++){
+            int table=num*i;
+            System.out.println(table);
+        }
+    }
+}
