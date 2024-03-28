79369bb0cbfb5409581dbba1145bb63cfbf7bab7
Gungun agrawal <agrawalgungun859@gmail.com>
2024-03-28 12:31:00 +0530

    even or odd

---
+++ src/BasicJava/Ifelse/EvenOrOdd.java
@@ -0,0 +1,17 @@
+package BasicJava.Ifelse;
+
+import java.util.Scanner;
+
+public class EvenOrOdd {
+    public static void main(String[] args) {
+        // To check the no. is even or odd
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter any number");
+        int number=input.nextInt();
+        if(number%2==0){
+            System.out.println("No. is even");
+        }
+        else
+            System.out.println("No. is odd");
+    }
+}
