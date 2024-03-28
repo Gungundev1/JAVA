ce50586d2d4350455c57b8edaba5b8dba0479c5f
Gungun agrawal <agrawalgungun859@gmail.com>
2024-03-28 22:05:10 +0530

    Prime or non prime

---
+++ src/BasicJava/Ifelse/PrimeOrNot.java
@@ -0,0 +1,28 @@
+package BasicJava.Ifelse;
+
+import java.util.Scanner;
+
+public class PrimeOrNot {
+    public static void main(String[] args) {
+        // To check the no. is prime or not
+        Scanner input=new Scanner(System.in);
+        int number= input.nextInt();
+        int count=0;
+        if (number==0||number==1){
+            System.out.printf("No is not prime");
+        }
+        else{
+            for(int i=2;i<=number;i++){
+                if(number%i==0){
+                    count++;
+                }
+            }
+            if (count == 1) {
+                System.out.println("No. is prime");
+            }
+            else{
+                System.out.println("No is not prime");
+            }
+        }
+    }
+}
