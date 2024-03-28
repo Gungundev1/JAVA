d3c45d447635451a583573904370d8c6e2a5cf70
Gungun agrawal <agrawalgungun859@gmail.com>
2024-03-28 17:00:58 +0530

    leap year

---
+++ src/BasicJava/Ifelse/LeapyearOrNot.java
@@ -0,0 +1,17 @@
+package BasicJava.Ifelse;
+
+import java.util.Scanner;
+
+public class LeapyearOrNot {
+    public static void main(String[] args) {
+        //To check year is leap year or not
+        Scanner input=new Scanner(System.in);
+        int year= input.nextInt();
+        if(year%4==0||(year%400==0&&year/4==0)){
+            System.out.println(year+" is leap year");
+        }
+        else
+            System.out.print(year+" is not leap year");
+       
+    }
+}
