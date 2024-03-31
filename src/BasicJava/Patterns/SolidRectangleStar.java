64dee91240332256f41ec76d868470cae37de6d2
Gungun agrawal <agrawalgungun859@gmail.com>
2024-03-31 21:56:22 +0530

    solid rectangle

---
+++ src/BasicJava/Patterns/SolidRectangleStar.java
@@ -0,0 +1,13 @@
+package BasicJava.Patterns;
+
+public class SolidRectangleStar {
+    public static void main(String[] args) {
+        // To print solid rectangle star
+        for (int i=0;i<3;i++){
+            for (int j =0;j<8;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
