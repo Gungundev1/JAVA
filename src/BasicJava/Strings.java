0e3fb3569357d27e167451a141b6f93e3b52b7fa
Gungun Agrawal <156339867+Gungandev@users.noreply.github.com>
2024-04-06 19:16:23 +0530

    Update Strings.java

--- src/BasicJava/Strings.java
+++ src/BasicJava/Strings.java
@@ -10,21 +10,37 @@ public class Strings {
         System.out.println(name1);
 //       functions of a string
         System.out.println("length of name is  "+name1.length());//return length of the string
+        
         System.out.println("in lowercase  "+name1.toLowerCase());//return string with lowercase
+        
         System.out.println("in uppercase  "+name1.toUpperCase());//return string with uppercase
+        
         System.out.println("before trim  "+name);//return string
+        
         System.out.println("after trim  "+name.trim());//return string with removing all spaces
+        
         System.out.println("substring is  "+name1.substring(2));//return substring with starting index
+        
         System.out.println("substring is  "+name1.substring(1,4));//return substring b/w stating to ending index
+        
         System.out.println("replacing  "+name1.replace('t','m'));//replace old character to new character
+        
         System.out.println("start with ni  "+name1.startsWith("ni"));//return true or false
+        
         System.out.println("ends with ni  "+name1.endsWith("ha"));//return true or false
+        
         System.out.println("print character  "+name1.charAt(2));//return a character of the index no
+        
         System.out.println(name1.indexOf("an"));//return a character from that index
+        
         System.out.println(name1.lastIndexOf("n"));//return first occurrence of the string
+        
         System.out.println(name.lastIndexOf("a",6));//return last occurrence of the string
+        
         System.out.println("if name is equal  "+name1.equals("tanisha"));//return true or false
+        
         System.out.println(name1.equalsIgnoreCase("Tanisha"));//return true or false , ignoring the lowercase or uppercase
+        
     }
     String[] obj=new String[5];
 }
