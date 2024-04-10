4a28542626579e31b400d235781d8f9f64c65e82
Gungun agrawal <agrawalgungun859@gmail.com>
2024-04-10 15:45:25 +0530

    update

--- src/BasicJava/Patterns/HollowFullPyramid.java
+++ src/BasicJava/Patterns/HollowFullPyramid.java
@@ -8,36 +8,12 @@ public class HollowFullPyramid {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter any no.");
         int n = input.nextInt();
-//        int i, j;
-//        for (i = 1; i <= n; i++) {
-//            for (j = 1; j <= (n - i); j++) {
-//                System.out.print(" ");
-//            }
-//
-//            for ( int k = 1; k <= i; k++) {
-//                if(k==1||k==n){
-//                    System.out.print("*");
-//                    System.out.print(" ");
-//                }
-//                System.out.print(" ");
-//
-//            }
-//            System.out.println();
-//        }
-//        for (i=1;i==n;i++){
-//            System.out.print("*");
-//        }
-        for (int i=0;i<=n;i++){
-           for (int j=1;j<=2*n;j++){
-               if (i+j==(n+1)||j-i==(n-1)||i==n){
-                   System.out.print("*");
-                   System.out.print(" ");
-               }
-               else{
-                   System.out.print(" ");
-               }
-           }
-            System.out.println();
+        int i, j;
+        for (i = 1; i <= n; i++) {
+            for (j = 1; j <= (n - i); j++) {
+                System.out.print(" ");
+            }
+
         }
     }
 }
