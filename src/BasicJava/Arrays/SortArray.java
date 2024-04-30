dd3f5bc396a7f565bd4b895e381fc5d6987c13ea
Gungun Agrawal <156339867+Gungundev1@users.noreply.github.com>
2024-04-30 21:42:59 +0530

    Update SortArray.java

--- src/BasicJava/Arrays/SortArray.java
+++ src/BasicJava/Arrays/SortArray.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class SortArray {
-    // To sort an array in ascending order
+    // To sort an array in ascending order.
     public static void main(String[] args) {
         System.out.println("Enter size of array:");
         Scanner input=new Scanner(System.in);
